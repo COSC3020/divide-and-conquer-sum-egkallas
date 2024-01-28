1148e7e9389a7b1266f8e08824b2ee844ae6fdbe
Evan Kallas <144737982+egkallas@users.noreply.github.com>
2024-01-28 12:23:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function divideAndConquerSum(a) {
+    a = divNConq(a);
     return a;
 }
+
+function divNConq(a) {
+    if (a.length == 0)
+        return 0;
+    else if (a.length <= 1) {
+        return a[0];}
+    else if (a.length == 2) {
+        return a[0] + a[1];}
+    else {
+        var third = Math.floor(a.length / 3);
+        var leftSum = divNConq(a.slice(0, third));
+        var midSum = divNConq(a.slice(third, 2 * third));
+        var rightSum = divNConq(a.slice(2 * third));
+        return leftSum + midSum + rightSum;
+    }
+}
+
